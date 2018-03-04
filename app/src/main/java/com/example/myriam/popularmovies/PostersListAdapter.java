6f46eb44a1712b1ca057fd2539efaa1870d107fd
myriamreda9 <myriam.dimitry94@gmail.com>
2018-03-05 00:13:02 +0200

    Basic

---
+++ app/src/main/java/com/example/myriam/popularmovies/PostersListAdapter.java
@@ -0,0 +1,69 @@
+package com.example.myriam.popularmovies;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Myriam on 3/3/2018.
+ */
+
+public class PostersListAdapter extends BaseAdapter {
+
+private Context context ;
+
+    public PostersListAdapter(Context context) {
+    this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(context);
+//            imageView.setLayoutParams(new GridView.LayoutParams(100, 150));
+              imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+//mock data
+private Integer[] mThumbIds = {
+        R.drawable.sample_2, R.drawable.sample_3,
+        R.drawable.sample_4, R.drawable.sample_5,
+        R.drawable.sample_6, R.drawable.sample_7,
+        R.drawable.sample_0, R.drawable.sample_1,
+        R.drawable.sample_2, R.drawable.sample_3,
+        R.drawable.sample_4, R.drawable.sample_5,
+        R.drawable.sample_6, R.drawable.sample_7,
+        R.drawable.sample_0, R.drawable.sample_1,
+        R.drawable.sample_2, R.drawable.sample_3,
+        R.drawable.sample_4, R.drawable.sample_5,
+        R.drawable.sample_6, R.drawable.sample_7
+};
+
+}
